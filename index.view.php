7053bbc8c9500fb0aa2a61c7e2b4eebd758d80ba
Antonio <aalaminosz@gmail.com>
2018-03-22 10:37:12 +0100

    deleted meta page and images

--- index.view.php
+++ index.view.php
@@ -74,7 +74,6 @@ if( isset($_SESSION['user_id']) ) {
             <ul>
                 <li><a class="link_btn" href="getUsers.php">Get users</a></li>
                 <li><a class="link_btn" href="newSentence.php">Add sentence</a></li>
-                <li><a class="link_btn" href="meta.html">Meta</a></li>
                 <li class="divider"></li>
                 <li><a class="link_btn" href="#">Highscores</a>
             </ul>
