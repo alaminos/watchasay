9187ffcc0e9ad617df63b4bd67cc8b26fdb1d2f6
sleuvins <sylvain.schellenberger@gmail.com>
2018-04-03 22:08:37 +0200

    Added default config file

--- pdo.php
+++ pdo.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('config.php');
+require_once('config.default.php');
 $statement = "'mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . "', '" . DB_USER . "','" . DB_PASS . "'";
 $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=blablapp;port=' . DB_PORT, DB_USER, DB_PASS);
 //See the "errors" folder for details.
