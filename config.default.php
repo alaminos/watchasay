a2969128e4c433fe01771b5501a1dc3cc4d6e995
sleuvins <sylvain.schellenberger@gmail.com>
2018-04-04 12:57:31 +0200

    Now working with only the config.default file

--- config.default.php
+++ config.default.php
@@ -1,11 +1,6 @@
 <?php
-	if(!defined('DB_HOST'))
-		define('DB_HOST', 'localhost');
-	if(!defined('DB_NAME'))
-		define('DB_NAME', 'blablapp');
-	if(!defined('DB_PORT'))
-		define('DB_PORT', '3306');
-	if(!defined('DB_USER'))
-		define('DB_USER', 'root');
-	if(!defined('DB_PASS'))
-		define('DB_PASS', '');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'blablapp');
+	define('DB_PORT', '3306');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
--- pdo.php
+++ pdo.php
@@ -1,7 +1,10 @@
 <?php
 
-require_once('config.php');
-require_once('config.default.php');
+if (file_exists('config.php'))
+	require_once('config.php');
+else
+	require_once('config.default.php');
+
 $statement = "'mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . "', '" . DB_USER . "','" . DB_PASS . "'";
 $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=blablapp;port=' . DB_PORT, DB_USER, DB_PASS);
 //See the "errors" folder for details.
